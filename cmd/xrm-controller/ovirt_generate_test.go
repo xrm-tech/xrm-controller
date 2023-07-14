14bc6545a25bff864c174d4486f9b46bf5cb1237
Michail Safronov <msaf1980@gmail.com>
2023-07-14 12:59:08 +0500

    ovirt: filter name

--- cmd/xrm-controller/ovirt_generate_test.go
+++ cmd/xrm-controller/ovirt_generate_test.go
@@ -78,3 +78,57 @@ func TestGenerateValidate(t *testing.T) {
 	// 	t.Fatal(err)
 	// }
 }
+
+func TestGenerateFiltering(t *testing.T) {
+	var err error
+
+	if xrm.Cfg.Listen, err = tests.GetFreeLocalAddr(); err != nil {
+		t.Fatal(err)
+	}
+	if xrm.Cfg.OVirtStoreDir, err = os.MkdirTemp("", "xrm-controller"); err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(xrm.Cfg.StoreDir)
+
+	request := "http://" + xrm.Cfg.Listen + "/ovirt/generate/test%2F..%2F..%2F..%2Fetc"
+	// fileName := path.Join(ovirtStoreDir, "test/disaster_recovery_vars.yml")
+
+	// create and start *fiber.App instance
+	xrm.Cfg.Logger = zerolog.New(os.Stdout)
+	xrm.Cfg.Users = map[string]string{"test1": "password1", "test2": "password2"}
+	app := xrm.RouterInit()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		wg.Done()
+		_ = app.Listen(xrm.Cfg.Listen)
+	}()
+	wg.Wait()
+	defer func() { _ = app.Shutdown() }()
+	time.Sleep(time.Millisecond * 10)
+
+	siteConfig := ovirt.GenerateVars{
+		PrimaryUrl:        "https://saengine2.localdomain/ovirt-engine/api",
+		PrimaryUsername:   "admin@ovirt@internal",
+		PrimaryPassword:   "password",
+		SecondaryUrl:      "https://saengine2.localdomain/ovirt-engine/api",
+		SecondaryUsername: "admin@ovirt@internal",
+		SecondaryPassword: "password",
+		StorageDomains: []ovirt.Storage{
+			{
+				PrimaryType:   "nfs",
+				PrimaryName:   "nfs_dom",
+				PrimaryPath:   "/nfs_dom_dr/",
+				PrimaryAddr:   "10.1.1.2",
+				SecondaryType: "nfs",
+				SecondaryName: "nfs_dom",
+				SecondaryPath: "/nfs_dom_dr2/",
+				SecondaryAddr: "10.1.2.3",
+			},
+		},
+	}
+	// run without parameters
+	if err := tests.DoGenerate(request, &siteConfig, "test2", "password2", http.StatusBadRequest, "name is invalid\n"); err == nil {
+		t.Fatal("mnust fail")
+	}
+}
